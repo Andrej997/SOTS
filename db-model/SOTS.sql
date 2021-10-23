8c9c89512ef3da2a53ceb3b03d098bd80424b212
Andrej997 <andrej.km997@gmail.com>
2021-10-23 23:53:54 +0200

    db-model - update

--- db-model/SOTS.sql
+++ db-model/SOTS.sql
@@ -34,11 +34,18 @@ CREATE TABLE "tests" (
 );
 
 CREATE TABLE "student_tests" (
+  "id" BIGSERIAL PRIMARY KEY,
   "user_id" bigint,
   "test_id" bigint,
   "took_test" boolean,
-  "grade_id" bigint,
-  PRIMARY KEY ("user_id", "test_id")
+  "points" float,
+  "grade_id" bigint
+);
+
+CREATE TABLE "tests_test_time" (
+  "test_id" bigint,
+  "test_time_id" bigint,
+  PRIMARY KEY ("test_id", "test_time_id")
 );
 
 CREATE TABLE "test_time" (
@@ -49,6 +56,8 @@ CREATE TABLE "test_time" (
 
 CREATE TABLE "grades" (
   "id" [pk],
+  "from_procentage" float,
+  "to_procentage" float,
   "label" text
 );
 
@@ -61,7 +70,21 @@ CREATE TABLE "questions" (
 CREATE TABLE "answers" (
   "id" BIGSERIAL PRIMARY KEY,
   "text_answer" text,
-  "question_id" bigint
+  "question_id" bigint,
+  "is_correct" boolean
+);
+
+CREATE TABLE "question_completed" (
+  "id" bigint PRIMARY KEY,
+  "student_tests_id" bigint,
+  "question_id" bigint,
+  "completed_persentage" fload
+);
+
+CREATE TABLE "user_subject" (
+  "subject_id" bigint,
+  "user_id" bigint,
+  PRIMARY KEY ("subject_id", "user_id")
 );
 
 ALTER TABLE "user_roles" ADD FOREIGN KEY ("role_id") REFERENCES "roles" ("id");
@@ -76,11 +99,21 @@ ALTER TABLE "student_tests" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id"
 
 ALTER TABLE "tests" ADD FOREIGN KEY ("creator_id") REFERENCES "users" ("id");
 
-ALTER TABLE "tests" ADD FOREIGN KEY ("test_time_id") REFERENCES "test_time" ("id");
+ALTER TABLE "tests_test_time" ADD FOREIGN KEY ("test_id") REFERENCES "tests" ("test_time_id");
+
+ALTER TABLE "tests_test_time" ADD FOREIGN KEY ("test_time_id") REFERENCES "test_time" ("id");
 
 ALTER TABLE "student_tests" ADD FOREIGN KEY ("grade_id") REFERENCES "grades" ("id");
 
 ALTER TABLE "questions" ADD FOREIGN KEY ("id") REFERENCES "tests" ("question_id");
 
 ALTER TABLE "answers" ADD FOREIGN KEY ("question_id") REFERENCES "questions" ("id");
+
+ALTER TABLE "question_completed" ADD FOREIGN KEY ("student_tests_id") REFERENCES "student_tests" ("id");
+
+ALTER TABLE "question_completed" ADD FOREIGN KEY ("question_id") REFERENCES "questions" ("id");
+
+ALTER TABLE "user_subject" ADD FOREIGN KEY ("subject_id") REFERENCES "subjects" ("id");
+
+ALTER TABLE "user_subject" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
 
