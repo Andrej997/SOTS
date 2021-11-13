db49f5f16f229ad35d42dd1fe433edff506c28d4
Andrej997 <andrej.km997@gmail.com>
2021-11-14 00:05:51 +0100

    select distinct ?Concept where {[] a ?Concept} LIMIT 100

---
+++ back-end/API/src/WebApi/Controllers/VirtuosoController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.WebApi.Controllers
+{
+    public class VirtuosoController : ApiControllerBase
+    {
+        [HttpGet]
+        [ApiExplorerSettings(GroupName = "v1")]
+        public async Task<IActionResult> GetQustionsInfo()
+        {
+            try
+            {
+                var client = new RestClient($"http://localhost:8890/sparql");
+                client.AddDefaultParameter("format", "application/json");
+                //client.AddDefaultParameter("format", "text/turtle");
+                client.AddDefaultParameter("query", "select distinct ?Concept where {[] a ?Concept} LIMIT 100");
+                client.Timeout = -1;
+                var request = new RestRequest(Method.POST);
+                IRestResponse response = client.Execute(request);
+                var content = response.Content;
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
