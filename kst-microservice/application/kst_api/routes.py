09838ccfb784e2d2a4a7033c7e1cb33ec278dc71
Andrej997 <andrej.km997@gmail.com>
2021-12-23 21:26:32 +0100

    kst microservice

--- kst-microservice/application/kst_api/routes.py
+++ kst-microservice/application/kst_api/routes.py
@@ -1,8 +1,18 @@
 # application/kst_api/routes.py
 from flask import jsonify, request, make_response
 from . import kst_api_blueprint
+import pandas as pd
+from learning_spaces.kst import iita
 
-
-@kst_api_blueprint.route('/api/message', methods=['GET'])
-def hello():
-    return 'Hello'
+#{'a': [1, 0, 1], 'b': [0, 1, 0], 'c': [0, 1, 1]}
+@kst_api_blueprint.route('/api/calculate', methods=['POST'])
+def calculate():
+    matrix = request.json
+    data_frame = pd.DataFrame(matrix)
+    try:
+        response = iita(data_frame, v=1)
+    except Exception:
+        return 'error'
+    
+    response['diff'] = response['diff'].tolist()
+    return jsonify(response)
