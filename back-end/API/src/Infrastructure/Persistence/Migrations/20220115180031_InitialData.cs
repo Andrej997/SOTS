d18d7be32f682c09259d334050c52a9472a0f6ec
Andrej997 <andrej.km997@gmail.com>
2022-01-18 21:24:33 +0100

    initial grades

--- back-end/API/src/Infrastructure/Persistence/Migrations/20220115180031_InitialData.cs
+++ back-end/API/src/Infrastructure/Persistence/Migrations/20220115180031_InitialData.cs
@@ -12,6 +12,13 @@ namespace API.Infrastructure.Persistence.Migrations
                 insert into project.roles (id, name) values (2, 'professor');
                 insert into project.roles (id, name) values (3, 'student');
 
+                insert into project.grades (id, label, from_procentage, to_procentage) values (1, '5', 0, 50);
+                insert into project.grades (id, label, from_procentage, to_procentage) values (2, '6', 51, 60);
+                insert into project.grades (id, label, from_procentage, to_procentage) values (3, '7', 61, 70);
+                insert into project.grades (id, label, from_procentage, to_procentage) values (4, '8', 71, 80);
+                insert into project.grades (id, label, from_procentage, to_procentage) values (5, '9', 81, 90);
+                insert into project.grades (id, label, from_procentage, to_procentage) values (6, '10', 91, 100);
+
                 insert into project.users (id, name, surname, username, password_hash, created_at) values (1, 'admin', 'admin', 'admin', '$2a$11$T0F92aa6MyQHNYJERrUzge4Teh5QsO9GljSREDDuW/Y8p3YHX02Ra', '{DateTime.UtcNow}');
                 insert into project.user_roles (user_id, role_id) values (1, 1);
             ";
